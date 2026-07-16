42ac0c3c7a7c729180389ecb2a5835339de65846
Sri10042006 <santhasrisubramani@gmail.com>
2026-07-16 10:15:29 +0530

    added

---
+++ starpattern/right_triangle_star.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a ,i,j;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<a;j++){
+            if(i>=j)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }  
+}
